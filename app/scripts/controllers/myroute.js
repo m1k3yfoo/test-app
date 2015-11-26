2cae82fce70bb5a002bf77b13a9a4cae40635079
m1k3yfoo <m1k3yfoo@gmail.com>
2015-11-26 16:56:40 -0500

    first commit

---
+++ app/scripts/controllers/myroute.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testAppApp.controller:MyrouteCtrl
+ * @description
+ * # MyrouteCtrl
+ * Controller of the testAppApp
+ */
+angular.module('testAppApp')
+  .controller('MyrouteCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
